3d0453231a72efcb3e931a6b40414b893e789d49
dongsu-iis <dongsu.workout@outlook.jp>
2020-05-16 00:41:22 +0900

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace API {
     public class Startup {
@@ -30,6 +24,8 @@ namespace API {
             services.AddDbContext<StoreContext> (x => {
                 x.UseSqlite (_config.GetConnectionString ("DefaultConnection"));
             });
+
+            services.AddScoped<IProductRepository, ProductRepository> ();
 
         }
 
