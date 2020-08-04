3b310a0a0a59d7bf48176bd2588c8c5c02956572
dongsu-iis <dongsu.workout@outlook.jp>
2020-08-05 01:25:11 +0900

    install redis

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API {
     public class Startup {
@@ -36,6 +37,12 @@ namespace API {
                 opt.AddPolicy ("CorsPolicy", policy => {
                     policy.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("https://localhost:4200");
                 });
+            });
+
+            services.AddSingleton<IConnectionMultiplexer> (c => {
+                var configuration = ConfigurationOptions.Parse (_config
+                    .GetConnectionString ("Redis"), true);
+                return ConnectionMultiplexer.Connect (configuration);
             });
 
         }
