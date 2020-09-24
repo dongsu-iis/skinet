c4432d2fef67d6429c8f3f48be8b976036daa730
dongsu <dongsu.workout@outlook.jp>
2020-09-24 14:57:59 +0900

    section 18 207

--- client/src/app/account/account-routing.module.ts
+++ client/src/app/account/account-routing.module.ts
@@ -2,7 +2,6 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
